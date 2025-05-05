4385678d238fc99173c535d55243e4e37e7b194b
samsonli333 <85639220+samsonli333@users.noreply.github.com>
2025-05-05 05:50:13 +0000

    feat:commit

--- chat_app2.py
+++ chat_app2.py
@@ -18,11 +18,9 @@ model = tf.keras.models.load_model('tdc.keras')
 st.markdown(
     """
     <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
+    #MainMenu {
+  visibility: hidden;
+}
     </style>
     """,
     unsafe_allow_html=True
